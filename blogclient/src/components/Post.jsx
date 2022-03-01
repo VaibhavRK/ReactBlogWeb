4deb6cd4e88ef838c8510cbec3fc671ea0425184
Son Goku <songoku546@dbz.com>
2022-03-01 13:20:26 +0530

    krdiya

--- blogclient/src/components/Post.jsx
+++ blogclient/src/components/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/PostCard.css';
 import Tilt from 'react-parallax-tilt';
+import ReactMarkdown from 'react-markdown';
 
 function Post({post}) {   
     const url = post.picture ? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
@@ -27,7 +28,7 @@ function Post({post}) {
                  <p style={{fontSize:'0.8rem'}}>Category: {post.categories}</p>
                  <h4 style={{fontSize:'1.4rem',marginTop:-16}}>{setElipseHeading(post.title)}</h4>
                  <p style={{fontSize:'0.8rem',marginTop:-6}} >Author: {setElipse(post.author)}</p>
-                 <p style={{marginTop:-15,textAlign:'center'}}> {setElipse(post.description)} </p>
+                 <p style={{marginTop:-15,textAlign:'center'}}> <ReactMarkdown>{setElipse(post.description)}</ReactMarkdown></p>
             </div>
         </div>
       </Tilt>
